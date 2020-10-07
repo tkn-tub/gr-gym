d580fa284fab81adb08aebc3471eb268ede6dfac
zubow <zubow@ilona.tkn.tu-berlin.de>
2020-10-07 16:14:10 +0200

    remote script

--- examples/rl-wifi-rt/grc/start_benchmark_ieee80211_wifi_loopback_zmq.sh
+++ examples/rl-wifi-rt/grc/start_benchmark_ieee80211_wifi_loopback_zmq.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+    echo "Please set name of remote node"
+    exit 1
+fi
+
+remote_addr=$1
+
+echo "Creating GRC to $remote_addr"
+
+cat benchmark_ieee80211_wifi_loopback_zmq_remote.grc | sed "s/localhost/$remote_addr/g" > tmp.grc
+grcc tmp.grc
 python3 benchmark_ieee80211_wifi_loopback_zmq.py > /tmp/out.log 2>&1
+
