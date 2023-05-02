ad60a84128612ceb7c0969d6b4f271c758fe9fe9
Sascha Rösler <s.roesler@tu-berlin.de>
2023-05-02 16:20:04 +0200

    fix merge setup file

--- grgym/setup.py
+++ grgym/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='grgym',
       version='0.0.3',
-      install_requires=['gymnasium>=0.2.3', 'ruamel.yaml', 'sh>=1.10.0', 'packaging>=20.0.0']
+      install_requires=['gymnasium>=0.2.3', 'ruamel.yaml', 'sh>=1.10.0', 'packaging>=20.0.0', 'python-box>=7.0.0']
 )
